3c242cd68f8aa57aa0d1343d1b17e018ccde1b4d
fillipe soranso <fillipe.soranso@gmail.com>
2025-04-25 19:20:41 -0300

    add collections - list

---
+++ collections-java-api/src/main/java/list/BasicOperations/Tarefa.java
@@ -0,0 +1,19 @@
+package main.java.list.BasicOperations;
+
+public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Descrição: " + descricao;
+    }
+    
+}
